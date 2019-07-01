d0dbd6240b3fd83e0e0f1674769849bb5fd9afa4
Kedi Kebba <kebbakedi@gmail.com>
2019-07-01 17:52:11 -0500

    Added Address Class and Person Class

---
+++ src/business/Address.java
@@ -0,0 +1,9 @@
+package business;
+
+public class Address {
+	private String state;
+	private String city;
+	private int zipCode;
+	private String street;
+
+}
---
+++ src/business/Person.java
@@ -0,0 +1,11 @@
+package business;
+
+public class Person {
+	
+	private String firstName;
+	private String lastName;
+	private String phone;
+	
+	private Address address;
+
+}
