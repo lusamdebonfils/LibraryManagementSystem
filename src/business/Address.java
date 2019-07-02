1c9fca8804bf1a307dae1ad84e5f54a305961dc1
Kedi Kebba <kebbakedi@gmail.com>
2019-07-02 07:35:29 -0500

    Added: 1. Modified Person and Address classes, added getters and Setters. 2. Modified Book class, commented out startup-code for book class and created a new one. 3. Added BookCopy copy class. 4. Added Author Class.
    Not Added:
    1. Association of Book and Author.
    2. Association of Person and Address.

--- src/business/Address.java
+++ src/business/Address.java
@@ -5,5 +5,47 @@ public class Address {
 	private String city;
 	private int zipCode;
 	private String street;
+	private Person person;
+	
+	Address(String state, String city, String street, int zipCode, Person person){
+		this.state = state;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+		person.setAddress(this);
+	}
 
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	
 }
