5ff8dcac7f83b04f17d981c5bf5b7429f095fb03
Kedi Kebba <kebbakedi@gmail.com>
2019-07-02 12:37:36 -0500

    Changed LibMember to inherit from Person class

--- src/business/LibraryMember.java
+++ src/business/LibraryMember.java
@@ -3,12 +3,14 @@ package business;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-public class LibraryMember implements Serializable {
+public class LibraryMember extends Person implements Serializable {
 	private CheckoutRecord record = new CheckoutRecord();
-	public LibraryMember(String name) {
-		this.name = name;
+	public LibraryMember(String firstName, String lastName, String phone, String memeberID) {
+		super(firstName, lastName, phone);
+		this.memeberID =  memeberID;
 	}
-	private String name;
+	
+	private String memeberID;
 	
 	public void checkout(LendableCopy copy, LocalDate checkoutDate, LocalDate dueDate) {
 		CheckoutRecordEntry entry = new CheckoutRecordEntry(copy, checkoutDate, dueDate);
@@ -17,7 +19,7 @@ public class LibraryMember implements Serializable {
 	}
 	
 	public String toString() {
-		return "Checkout record for library member " + name + ": " + record;
+		return "Checkout record for library member " + memeberID + ": " + record;
 	}
 	
 	private static final long serialVersionUID = -2226197306790714013L;
